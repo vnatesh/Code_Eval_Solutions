7ffa8c0cc116e8eeb534cd776c71ef95925ea9c2
vnatesh <vikas.natesh@gmail.com>
2015-03-24 14:41:18 -0400

    Update prime_palindrome.py

--- Easy/prime_palindrome.py
+++ Easy/prime_palindrome.py
@@ -1,3 +1,24 @@
+
+"""
+PRIME PALINDROME
+
+
+Write a program which determines the largest prime palindrome less than 1000.
+
+INPUT SAMPLE:
+
+There is no input for this program.
+
+OUTPUT SAMPLE:
+
+Print to stdout the largest prime palindrome less than 1000.
+
+929
+
+"""
+
+# Trial-division method
+
 import math
 def ispalindrome(s):
     if len(s)<=1: return True
