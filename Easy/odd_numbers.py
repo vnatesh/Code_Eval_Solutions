8e73a3f78ba0264634d56cbe5587bd5339318128
vnatesh <vikas.natesh@gmail.com>
2015-03-24 18:00:31 -0400

    Create odd_numbers.py

---
+++ Easy/odd_numbers.py
@@ -0,0 +1,23 @@
+"""
+
+ODD NUMBERS
+CHALLENGE DESCRIPTION:
+
+
+Print the odd numbers from 1 to 99.
+
+INPUT SAMPLE:
+
+There is no input for this program.
+
+OUTPUT SAMPLE:
+
+Print the odd numbers from 1 to 99, one number per line. 
+
+"""
+
+import sys
+
+for i in range(1,100):
+    if i%2>0:
+        print i
